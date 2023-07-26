57a13434e40a7b8d69fee01c223ecc11a022e580
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2023-07-26 06:37:46 +0000

    fix script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# 定义输出绿色文字的函数
+green_echo() {
+    echo -e "\033[32m$1\033[0m"
+}
+
 ######################
 # Install Docker
 ######################
 
-echo "Installing Docker..."
+green_echo "Installing Docker..."
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
@@ -24,25 +29,23 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo usermod -aG docker $(whoami)
-newgrp docker
-
-echo "Installing Docker... Done"
-
+# newgrp docker
+green_echo "Installing Docker... Done"
 
 ######################
 # Install Docker Compose
 ######################
-echo "Installing Docker Compose..."
+green_echo "Installing Docker Compose..."
 sudo apt-get install -y docker-compose
-echo "Installing Docker Compose... Done"
+green_echo "Installing Docker Compose... Done"
 
 
 ######################
 # Install Git
 ######################
-echo "Installing Git..."
+green_echo "Installing Git..."
 sudo apt-get install -y git
-echo "Installing Git... Done"
+green_echo "Installing Git... Done"
 
 
 ######################
@@ -57,12 +60,12 @@ cd ~/e5-toolkit
 docker-compose pull
 
 ######################
-echo "[Help] 接下来请执行的操作是："
-echo "[1] 编辑 config.env 文件，命令：vim config.env 或者 nano config.env，退出方法是：按下 Esc 键，然后输入 :wq 回车(vim) / crtl+x Y回车(nano)"
-echo "[2] 然后执行 docker-compose up -d，这个操作会启动所有的容器"
-echo "[3] 安装Nginx，命令：sudo apt-get install -y nginx"
-echo "[4] 编辑 nginx.conf 文件，然后覆盖到 /etc/nginx/nginx.conf，命令是：sudo vim /etc/nginx/nginx.conf 之后执行 sudo cp ~/e5-toolkit/nginx.conf /etc/nginx/nginx.conf"
-echo "[5] 重启Nginx，命令：sudo systemctl restart nginx"
-echo "如果你不知道如何编辑文件，请参考：https://www.runoob.com/linux/linux-vim.html"
+green_echo "[Help] 接下来请执行的操作是："
+green_echo "[1] 编辑 config.env 文件，命令：vim config.env 或者 nano config.env，退出方法是：按下 Esc 键，然后输入 :wq 回车(vim) / crtl+x Y回车(nano)"
+green_echo "[2] 然后执行 docker-compose up -d，这个操作会启动所有的容器"
+green_echo "[3] 安装Nginx，命令：sudo apt-get install -y nginx"
+green_echo "[4] 编辑 nginx.conf 文件，然后覆盖到 /etc/nginx/nginx.conf，命令是：sudo vim /etc/nginx/nginx.conf 之后执行 sudo cp ~/e5-toolkit/nginx.conf /etc/nginx/nginx.conf"
+green_echo "[5] 重启Nginx，命令：sudo systemctl restart nginx"
+green_echo "如果你不知道如何编辑文件，请参考：https://www.runoob.com/linux/linux-vim.html"
 
 
